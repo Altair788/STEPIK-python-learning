4748a3492666ea312ee0e0ce7f7a96b62d652c7f
Eduard Slobodyanik <112650697+Altair788@users.noreply.github.com>
2022-11-08 21:14:24 +0300

    in process

--- 13.4.py
+++ 13.4.py
@@ -1,3 +1,22 @@
+def quick_merge(list1, list2):
+    result = []
+    p1 = 0
+    p2 = 0
+    while p1 < len(list1) and p2 < len(list2):
+        if list1[p1] <= list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+        else:
+            result.append((list2[p2]))
+            p2 += 1
+    if p1 < len(list1):
+        result += list1[p1:]
+    if p2 < len(list2):
+        result += list2[p2:]
+
+    return result
+
+total_list = []
 n = int(input())
 for i in range(n):
-    s = input().split()
+    [int(i) for i in input().split()]
