da72a31f64ffd38fe701fc97533e346ff2f276b5
Eduard Slobodyanik <112650697+Altair788@users.noreply.github.com>
2022-11-12 14:05:14 +0300

    Next Prime 🌶️🌶️ Напишите функцию get_next_prime(num), которая принимает в качестве аргумента натуральное число num и возвращает первое простое число большее числа num.
    Примечание 1. Используйте функцию is_prime() из предыдущей задачи.

--- 13.5.3.py
+++ 13.5.3.py
@@ -1,9 +1,17 @@
+def is_prime(num):
+    counter = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            counter += 1
+    if counter == 2:
+        return True
+    else:
+        return False
 # объявление функции
 def get_next_prime(num):
     num += 1
-    for i in range(2, num):
-        if num % i == 0:
-            return get_next_prime(num)
+    while is_prime(num) == False:
+        num += 1
     return num
 
 
