1a2f42d5b576626c835b88a09f86321e39666fa2
Eduard Slobodyanik <112650697+Altair788@users.noreply.github.com>
2022-11-21 19:32:37 +0300

    Правильная скобочная последовательность ️
    Напишите функцию is_correct_bracket(text), которая принимает в качестве аргумента непустую строку text, состоящую из символов ( и ) и возвращает значение True если поступившая на вход строка является правильной скобочной последовательностью и False в противном случае.
    
    Примечание 1. Правильной скобочной последовательностью называется строка, состоящая только из символов ( и ), где каждой открывающей скобке найдется парная закрывающая скобка.

--- 13.5.8.py
+++ 13.5.8.py
@@ -1,20 +1,14 @@
 #   количество должно быть четным
 def is_correct_bracket(text):
-    if len(text) % 2 != 0:
-        return False
-    if len(text) % 2 == 0:
-        if text[0] == ')' or text[::-1] == '(':
-            return False
-    counter = 0
-    for i in text:
-        if i == '(':
-            counter += 1
+    for i in range(len(text)):
+        if '()' in text:
+            text = text.replace('()', '')
         else:
-            counter -= 1
-    if counter == 0:
-        return True
-    else:
-        return False
+            if len(text) == 0:
+                return True
+            else:
+                return False
+
 
 txt = input()
 
