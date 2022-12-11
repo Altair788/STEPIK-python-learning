246f6c5f85ade748c192396fda6dd62860ba6117
Eduard Slobodyanik <112650697+Altair788@users.noreply.github.com>
2022-12-11 19:49:31 +0300

    guess_game

--- 15.2.1.py
+++ 15.2.1.py
@@ -1,9 +1,14 @@
+
 #   алгоритм игры
-def ugaday_ka(a):
+def ugaday_ka():
     import random
     x = random.randrange(0, 101)
     print(x)
-    a = int(input('Я загадал число от 0 до 100. Попробуй его угадать. Введи свой вариант   '))
+    a = (input('Введи свой вариант числа     '))
+    if a.isdigit() is False:
+        digit()
+    else:
+        a = int(a)
     for i in range(1):
         while a != x:
             if a > x:
@@ -15,7 +20,7 @@ def ugaday_ka(a):
         if a == x:
             answer = input('Ты угадал! Поздравляю!!! Хочешь попробовать еще раз?  ' )
             if answer.lower() == 'да':
-                ugaday_ka(a)
+                ugaday_ka()
             else:
                 disagree(answer)
 
@@ -26,14 +31,28 @@ def disagree(answer):
             print('Возвращайся, когда передумаешь')
             answer = input('А может разок сыграем? "да" или "нет" ?  ')
             if answer.lower() == 'да':
-                ugaday_ka(a)
+                ugaday_ka()
             elif answer.lower() == 'нет':
                 print('Хорошо - хорошо :) Возвращайся, когда передумаешь')
 
+
+#   правила игры
+def rules():
+    print('Давай ознакомлю тебя с правилами игры.')
+    print('Я загадаю число от 0 до 100, а ты будешь его отгадывать.')
+    input('А теперь напиши что-нибудь, чтобы я был уверен, что ты понял правила игры ;-)    ')
+
+#   проверка правильности
+def digit():
+    print('Это не число :(')
+    print('Введи, пожалуйста, число')
+    ugaday_ka()
+
 answer = input('Привет! Меня зовут Эдуард и я написал код для игры "Угадай-ка". Хочешь в нее поиграть?   ')
-a = ''
+
 for i in range(1):
     if answer.lower() == 'да':
-        ugaday_ka(a)
+        rules()
+        ugaday_ka()
     else:
         disagree(answer)
