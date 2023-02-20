9f4574cfc6bb82041ef56716ab1a38f92372c474
Eduard Slobodyanik <112650697+Altair788@users.noreply.github.com>
2023-02-20 19:01:17 +0300

    Create 15.4.py

---
+++ 15.4.py
@@ -0,0 +1,17 @@
+    #  генератор случайных паролей
+import random
+digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+lowercase_letters = [a for a in 'abcdefghijklmnopqrstuvwxyz']
+uppercase_letters = [b for b in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+punctuation = [c for c in '!#$%&*+-=?@^_']
+chars = ''
+input('Какое количество паролей для генерации вам необходимо?  ')
+input('Какая длина одного пароля?     ')
+input('Включать ли цифры 0123456789?  ')
+input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?   ')
+input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?    ')
+input('Включать ли символы !#$%&*+-=?@^_?    ')
+input('Исключать ли неоднозначные символы il1Lo0O?   ')
+
+
+
